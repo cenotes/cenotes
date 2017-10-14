668b238bcde65e348451215b317592b3c91a1da6
John Paraskevopoulos <io.paraskev@gmail.com>
2017-10-14 03:10:12 +0300

    Add extra options/func to Notes initilizer
    - Adds init options (with defaults) for `max_visits`, `visits_count` and
    `expiration_date`
    - Adds property `iso_expiration_date`
    - Enforces payload to be bytes

--- cenotes/models.py
+++ cenotes/models.py
@@ -1,15 +1,29 @@
+from datetime import date
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 
 class Note(db.Model):
+    DEFAULT_MAX_VISITS = 1
+    DEFAULT_VISITS_COUNT = 0
 
     id = db.Column(db.Integer, primary_key=True)
-    max_visits = db.Column(db.Integer, default=1)
-    visits_count = db.Column(db.Integer, default=0)
+    max_visits = db.Column(db.Integer, default=DEFAULT_MAX_VISITS)
+    visits_count = db.Column(db.Integer, default=DEFAULT_VISITS_COUNT)
     payload = db.Column(db.Binary, nullable=False)
     expiration_date = db.Column(db.Date)
 
-    def __init__(self, payload):
-        self.payload = payload
+    def __init__(self, payload, max_visits=DEFAULT_MAX_VISITS,
+                 visits_count=DEFAULT_VISITS_COUNT, expiration_date=None):
+        try:
+            self.payload = payload.encode()
+        except AttributeError:
+            self.payload = payload
+        self.expiration_date = expiration_date or date.today()
+        self.max_visits = max_visits
+        self.visits_count = visits_count
+
+    @property
+    def iso_expiration_date(self):
+        return self.expiration_date.isoformat()
