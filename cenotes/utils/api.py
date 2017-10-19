8fd3294d6759a723ca50816ebfe1214ae748f057
John Paraskevopoulos <io.paraskev@gmail.com>
2017-10-19 17:18:42 +0300

    Add management commands
    - Adds command that prints the response/request format in json
    - Adds command that prints the available endpoint, the routes and the
    accepted methods

--- cenotes/utils/api.py
+++ cenotes/utils/api.py
@@ -2,17 +2,21 @@ import json
 from cenotes.utils import CENParams
 
 
+def craft_response(enote, error, key, payload, plaintext, success):
+    return {"success": success if not error else False,
+            "error": error,
+            "key": key,
+            "plaintext": plaintext,
+            "payload": payload,
+            "expiration_date": enote.iso_expiration_date if enote else "",
+            "max_visits": enote.max_visits if enote else ""}
+
+
 def craft_json_response(
         success=True, error="", enote=None, plaintext="", key="", payload=""):
 
     return json.dumps(
-        {"success": success if not error else False,
-         "error": error,
-         "key": key,
-         "plaintext": plaintext,
-         "payload": payload,
-         "expiration_date": enote.iso_expiration_date if enote else "",
-         "max_visits": enote.max_visits if enote else ""})
+        craft_response(enote, error, key, payload, plaintext, success))
 
 
 def get_request_params(request_params):
--- manage.py
+++ manage.py
@@ -1,9 +1,78 @@
+from datetime import date
+import json
+import functools
+from urllib.parse import unquote
+from flask_script import Manager
+from flask_migrate import MigrateCommand
 from cenotes import create_app
-from flask_migrate import MigrateCommand, Manager
+from cenotes.models import Note
+from cenotes.utils import CENParams
+from cenotes.utils.api import craft_response
 
 manager = Manager(create_app)
 manager.add_option("-c", "--config", dest="app_settings", required=False)
 manager.add_command('db', MigrateCommand)
+
+
+def show_json_request_format(indent=False):
+    func = functools.partial(json.dumps, indent=4) if indent else json.dumps
+    return func(CENParams(plaintext="", key="",
+                          expiration_date=date.today().isoformat(),
+                          visits_count=0, max_visits=0).__dict__)
+
+
+def show_json_response_format(indent=False):
+    func = functools.partial(json.dumps, indent=4) if indent else json.dumps
+    return func(craft_response(
+        error="", success=True, plaintext="", key="", payload="ciphertext",
+        enote=Note(payload="ciphertext", expiration_date=date.today())))
+
+
+def list_url_endpoints():
+    output = []
+    for rule in manager.app.url_map.iter_rules():
+        if rule.endpoint == "static":
+            continue
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = "[{0}]".format(arg)
+
+        methods = ','.join(rule.methods)
+        line = unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, rule))
+        output.append(line)
+    return output
+
+
+@manager.option('--response', dest="response", action="store_true", default=False)
+@manager.option('--request', dest="request", action="store_true", default=False)
+@manager.option('--both', dest="both", action="store_true", default=False)
+def api(response, request, both):
+    def response_format():
+        print("Response format is always like this:\n"
+              "(Some fields may be left empty depending on endpoint call)")
+        print(show_json_response_format(indent=True))
+        print("")
+
+    def request_format():
+        print("Request format is always like this:\n"
+              "(Some fields may be left empty depending on endpoint call)")
+        print(show_json_request_format(indent=True))
+        print("")
+
+    if both:
+        response = request = True
+
+    if response:
+        response_format()
+    if request:
+        request_format()
+
+
+@manager.command
+def routes():
+    for line in sorted(list_url_endpoints()):
+        print(line)
+
 
 if __name__ == '__main__':
     manager.run()
