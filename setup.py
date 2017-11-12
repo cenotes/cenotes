c767f9246c5296c5b5ef6670f8c96e99c998002d
John Paraskevopoulos <io.paraskev@gmail.com>
2017-11-12 13:21:59 +0200

    Bump version: 0.5.2 → 0.5.3

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = [
 
 setup(
     name='cenotes',
-    version='0.5.2',
+    version='0.5.3',
     description="Cryptographical Expendable Notes",
     long_description=readme + '\n\n' + history,
     author="John Paraskevopoulos",
