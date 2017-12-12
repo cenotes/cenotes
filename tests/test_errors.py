1159e8e39bd43153e5f42bc1d68a4f3505979dd9
John Paraskevopoulos <io.paraskev@gmail.com>
2017-12-12 17:55:41 +0200

    Fallback 405 errors to 400

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -8,3 +8,15 @@ def test_404(client):
     assert response.json["plaintext"] == ""
     assert response.json["expiration_date"] == ""
     assert response.json["max_visits"] == ""
+
+
+def test_400(client):
+    response = client.get("/notes/encrypt/")
+    assert response.status_code == 400
+    assert response.json["success"] is False
+    assert response.json["error"] != ""
+    assert response.json["key"] == ""
+    assert response.json["payload"] == ""
+    assert response.json["plaintext"] == ""
+    assert response.json["expiration_date"] == ""
+    assert response.json["max_visits"] == ""
